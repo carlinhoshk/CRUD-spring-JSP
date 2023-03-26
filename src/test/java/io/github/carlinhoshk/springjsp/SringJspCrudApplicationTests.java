df51767bb294302dc36b50454df4c89f77829a27
carlinhoshk <carlosmdohk@gmail.com>
2023-03-26 18:05:12 -0300

    primeiro commit

---
+++ src/test/java/io/github/carlinhoshk/springjsp/SringJspCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.carlinhoshk.springjsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SringJspCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
